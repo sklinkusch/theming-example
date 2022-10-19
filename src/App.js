569879286b60fd24b0666056f66614493bc5512b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-10-19 20:20:37 +0200

    add checkbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const [isChecked, setChecked] = useState(false)
+  const toggleChecked = () => {
+    setChecked(!isChecked)
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={`App-header ${isChecked ? "light-theme" : "dark-theme" }`}>
+          <input type="checkbox" onChange={toggleChecked} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
